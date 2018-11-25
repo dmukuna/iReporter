6883d6e55fefcd7a7b54be606ecb0c55d9af529c
danxidas <danielmukuna.k@outlook.com>
2018-11-25 14:22:30 +0000

    [updated #162104082] create red-flag page

---
+++ ui/js/location.js
@@ -0,0 +1,37 @@
+<script>
+  var x = document.getElementById("demo");
+
+  function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            x.innerHTML = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            x.innerHTML = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            x.innerHTML = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            x.innerHTML = "An unknown error occurred."
+            break;
+    }
+  }
+
+  function showPosition(position) {
+    var latlon = position.coords.latitude + "," + position.coords.longitude;
+
+    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=700x300&sensor=false&key=AIzaSyDoEF_h66DXyPIRQjFb3D3RKp1kX8pN7DU ";
+
+    document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
+  }
+
+  function getLocation() {
+      if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(showPosition, showError);
+      } else {
+          x.innerHTML = "Geolocation is not supported by this browser.";
+      }
+  }
+
+</script>
