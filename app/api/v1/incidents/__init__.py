6316d159458c7658fa94da21b1efaba261fc02ff
danxidas <danielmukuna.k@outlook.com>
2018-11-30 09:47:55 +0000

    [finishes #162227964] finish creating an endpoint for editing attributes in red-flags

--- app/api/v1/incidents/__init__.py
+++ app/api/v1/incidents/__init__.py
@@ -2,7 +2,7 @@
 from flask import Blueprint
 #pylint: disable=import-error
 from flask_restful import Api, Resource
-from .views import RedFlags, RedFlag #RedFlagAttr
+from .views import RedFlags, RedFlag, RedFlagAttr
 
 
 VERSION_ONE = Blueprint('api_v1', __name__, url_prefix='/api/v1')
@@ -10,4 +10,4 @@ API = Api(VERSION_ONE)
 
 API.add_resource(RedFlags, '/red-flags')
 API.add_resource(RedFlag, '/red-flags/<int:flag_id>')
-# API.add_resource(RedFlagAttr, '/red-flags/<int:flag_id>/<string:attr>')
+API.add_resource(RedFlagAttr, '/red-flags/<int:flag_id>/<string:attr>')
