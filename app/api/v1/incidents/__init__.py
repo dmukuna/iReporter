835afe6a59f48aeba8ee33b2cffb6b16c44ee361
danxidas <danielmukuna.k@outlook.com>
2018-12-01 07:10:38 +0000

    [finishes #162227796]

---
+++ app/api/v1/incidents/__init__.py
@@ -0,0 +1,13 @@
+"""v1 package"""
+from flask import Blueprint
+#pylint: disable=import-error
+from flask_restful import Api, Resource
+#from .views import RedFlags, RedFlag, RedFlagAttr
+
+
+VERSION_ONE = Blueprint('api_v1', __name__, url_prefix='/api/v1')
+API = Api(VERSION_ONE)
+
+# API.add_resource(RedFlags, '/red-flags')
+# API.add_resource(RedFlag, '/red-flags/<int:flag_id>')
+# API.add_resource(RedFlagAttr, '/red-flags/<int:flag_id>/<string:attr>')
