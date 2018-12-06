361e03222f9b832fc035a3ff4311db0bde3618db
danxidas <danielmukuna.k@outlook.com>
2018-12-06 13:37:17 +0000

    [finishes #162474137] fixed issues in the application

--- app/api/v2/users/__init__.py
+++ app/api/v2/users/__init__.py
@@ -1,13 +1,13 @@
-"""v2 package"""
-from flask import Blueprint
-#pylint: disable=import-error
-from flask_restful import Api, Resource
-from .views import Users, User, UserAttr
-
-
-VERSION_TWO = Blueprint('api_v2', __name__, url_prefix='/api/v2')
-API = Api(VERSION_TWO)
-
-API.add_resource(Users, '/users')
-API.add_resource(User, '/users/<int:flag_id>')
-API.add_resource(UserAttr, '/users/<int:flag_id>/<string:attr>')
+# """v2 package"""
+# from flask import Blueprint
+# #pylint: disable=import-error
+# from flask_restful import Api, Resource
+# from .views import Users, User, UserAttr
+#
+#
+# VERSION_TWO = Blueprint('api_v2', __name__, url_prefix='/api/v2')
+# API = Api(VERSION_TWO)
+#
+# API.add_resource(Users, '/users')
+# API.add_resource(User, '/users/<int:u_id>')
+# API.add_resource(UserAttr, '/users/<int:flag_id>/<string:attr>')
